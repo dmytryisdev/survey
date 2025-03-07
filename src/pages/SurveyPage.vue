803a8effb6405e32cbfc3a8970952da1bce7c430
Andrew <aprosvetkin.exceedteam@gmail.com>
2025-03-07 15:41:22 +0300

    questionary model

--- src/pages/SurveyPage.vue
+++ src/pages/SurveyPage.vue
@@ -1,52 +1,47 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { onMounted, ref } from 'vue';
+import { useRouter } from 'vue-router';
+import { fetchQuestionnaire } from '~/api/Questionnaire';
+import QuestionnaireModel from '~/models/QuestionaryModel';
+const router = useRouter();
 
-const router = useRouter()
+const list = ref<QuestionnaireModel[]>([]);
 
-const list = ref([
-  { id: 123, name: 'Название опроса' },
-  { id: 124, name: 'Название опроса' },
-  { id: 125, name: 'Название опроса' },
-  { id: 126, name: 'Название опроса' },
-  { id: 127, name: 'Название опроса' },
-  { id: 128, name: 'Название опроса' },
-  { id: 129, name: 'Название опроса' },
-  { id: 130, name: 'Название опроса' },
-])
+const getQuestions = async () => {
+  try {
+    list.value = await fetchQuestionnaire();
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-const handleRemove = (id: number) => {
-  list.value = list.value.filter(item => item.id !== id)
-}
+const handleRemove = (id: string) => {
+  list.value = list.value.filter(item => item.id !== id);
+};
+
+onMounted(() => getQuestions());
 </script>
 
 <template>
   <ul>
-    <li
-      v-for="item in list"
-      :key="item.id"
-      class="border-b-2 border-b-gray-100"
-    >
+    <li v-for="item in list" :key="item.id" class="border-b-2 border-b-gray-100">
       <RouterLink
         :to="{ name: 'survey-parameters', params: { id: item.id } }"
         class="flex items-center justify-between p-3"
       >
-        <span>{{ item.name }}</span>
+        <span>{{ item.title }}</span>
         <div class="flex items-center justify-between gap-4">
           <button
             class="cursor-pointer"
             @click.prevent="router.push({ name: 'survey-parameters', params: { id: item.id } })"
-          >Редактировать</button>
-          <button
-            class="cursor-pointer"
-            @click.prevent="handleRemove(item.id)"
-          >Удалить</button>
+          >
+            Редактировать
+          </button>
+          <button class="cursor-pointer" @click.prevent="handleRemove(item.id)">Удалить</button>
         </div>
       </RouterLink>
     </li>
   </ul>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
